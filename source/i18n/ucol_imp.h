1ff1469015d7dff6af1d84c09bf3fe4e6cbf4c11
Unknown User <cvs@svn.icu-project.org>
2001-06-13 21:26:51 +0000

    This commit was manufactured by cvs2svn to create tag 'release-1-8-1'.
    git-svn-id: http://source.icu-project.org/repos/icu/icu/tags/release-1-8-1@4993 0a8b766c-62c9-45f9-954d-7f2943c9ead0

--- source/i18n/ucol_imp.h
+++ source/i18n/ucol_imp.h
@@ -71,8 +71,8 @@ Note 3 is the minimum value for Thai collation to work correctly.
 #define UCOL_UNSAFECP_TABLE_MASK 0x1fff
 
 
-#define UCOL_RUNTIME_VERSION 1
-#define UCOL_BUILDER_VERSION 1
+#define UCOL_RUNTIME_VERSION 2
+#define UCOL_BUILDER_VERSION 2
 
 /* flags bits for collIterate.flags       */
 /*                                        */
@@ -185,8 +185,6 @@ struct incrementalContext {
 #define UCOL_CASE_SHIFT_START 7
 
 #define UCOL_IGNORABLE 0
-#define UCOL_IMPLICIT_SURROGATE_LAST_CE_MASK     0x80200080
-#define UCOL_IMPLICIT_MISCELLANEOUS_LAST_CE_MASK 0x04000083
 
 /* get weights from a CE */
 #define UCOL_PRIMARYORDER(order) (((order) & UCOL_PRIMARYORDERMASK)>> UCOL_PRIMARYORDERSHIFT)
@@ -266,7 +264,7 @@ struct incrementalContext {
 * @param result size of the longest expansion with argument collation element
 *        as the last element
 */
-#define UCOL_GETMAXEXPANSION(coll, order, result) {                  \
+#define UCOL_GETMAXEXPANSION(coll, order, result) {                          \
   const uint32_t *start;                                                     \
   const uint32_t *limit;                                                     \
   const uint32_t *mid;                                                       \
@@ -287,8 +285,7 @@ struct incrementalContext {
   else if (*limit == order) {                                                \
          result = *(coll->expansionCESize + (limit - coll->endExpansionCE)); \
        }                                                                     \
-       else if (order ^ UCOL_IMPLICIT_SURROGATE_LAST_CE_MASK == 0 ||         \
-                order ^ UCOL_IMPLICIT_MISCELLANEOUS_LAST_CE_MASK == 0) {     \
+       else if ((order & 0xFFFF) == 0x00C0) {                                \
               result = 2;                                                    \
             }                                                                \
             else {                                                           \
